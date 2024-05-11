109bcafd511033e8f81157033de244e519fbfa7c
xiewuzhiying <HoyhCreate@outlook.com>
2024-05-11 12:36:47 +0800

    em

--- fabric/src/main/java/io/github/xiewuzhiying/vs_addition/fabric/mixin/computercraft/ItemPocketComputerMixin.java
+++ fabric/src/main/java/io/github/xiewuzhiying/vs_addition/fabric/mixin/computercraft/ItemPocketComputerMixin.java
@@ -7,7 +7,6 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.Container;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
