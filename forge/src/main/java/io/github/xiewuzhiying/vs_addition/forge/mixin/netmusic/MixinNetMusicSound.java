4c6854f3fa8e560643b2af61cd942f6b0d333623
xiewuzhiying <HoyhCreate@outlook.com>
2024-06-09 17:43:49 +0800

    Net Music compat

---
+++ forge/src/main/java/io/github/xiewuzhiying/vs_addition/forge/mixin/netmusic/MixinNetMusicSound.java
@@ -0,0 +1,59 @@
+package io.github.xiewuzhiying.vs_addition.forge.mixin.netmusic;
+
+import com.github.tartaricacid.netmusic.client.audio.NetMusicSound;
+import io.github.xiewuzhiying.vs_addition.util.transformUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.sounds.AbstractTickableSoundInstance;
+import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.phys.Vec3;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.valkyrienskies.mod.common.VSGameUtilsKt;
+
+import java.net.URL;
+
+@Mixin(NetMusicSound.class)
+public abstract class MixinNetMusicSound extends AbstractTickableSoundInstance {
+    @Shadow(remap = false) @Final private BlockPos pos;
+
+    @Unique
+    private boolean vs_addition$isOnShip = false;
+
+    protected MixinNetMusicSound(SoundEvent arg, SoundSource arg2) {
+        super(arg, arg2);
+    }
+
+    @Inject(
+            method = "<init>",
+            at = @At("RETURN")
+    )
+    private void set(BlockPos pos, URL songUrl, int timeSecond, CallbackInfo ci) {
+        if(VSGameUtilsKt.isBlockInShipyard(Minecraft.getInstance().level, this.pos)) {
+            this.vs_addition$isOnShip = true;
+            Vec3 vec = VSGameUtilsKt.toWorldCoordinates(Minecraft.getInstance().level, transformUtils.getCenterOf(this.pos));
+            this.x = vec.x;
+            this.y = vec.z;
+            this.z = vec.z;
+        }
+    }
+
+    @Inject(
+            method = "tick",
+            at = @At("HEAD")
+    )
+    private void updatePos(CallbackInfo ci) {
+        if(this.vs_addition$isOnShip) {
+            Vec3 vec = VSGameUtilsKt.toWorldCoordinates(Minecraft.getInstance().level, transformUtils.getCenterOf(this.pos));
+            this.x = vec.x;
+            this.y = vec.z;
+            this.z = vec.z;
+        }
+    }
+}
