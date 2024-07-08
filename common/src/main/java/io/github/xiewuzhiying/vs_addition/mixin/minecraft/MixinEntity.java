b540c5052d20d25575c6e4282cd8f68d42450558
xiewuzhiying <HoyhCreate@outlook.com>
2024-07-08 14:49:27 +0800

    more things about ship mounting entity Minimap rotation plus the ship's yaw angle. (Xaero's Minimap JourneyMap, FTB Chunks and VoxelMap Updated.) Automatically rotate the camera when riding/removing a ship mounting entity.

---
+++ common/src/main/java/io/github/xiewuzhiying/vs_addition/mixin/minecraft/MixinEntity.java
@@ -0,0 +1,57 @@
+package io.github.xiewuzhiying.vs_addition.mixin.minecraft;
+
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.Level;
+import org.joml.Matrix4d;
+import org.joml.Vector3d;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.valkyrienskies.core.api.ships.Ship;
+import org.valkyrienskies.mod.common.VSGameUtilsKt;
+
+@Mixin(Entity.class)
+public abstract class MixinEntity {
+    @Shadow public abstract float getYRot();
+
+    @Shadow public abstract void setYRot(float yRot);
+
+    @Shadow public Level level;
+
+    @Inject(
+            method = "removeVehicle",
+            at = @At("HEAD")
+    )
+    private void addShipYaw(CallbackInfo ci) {
+        Ship ship = VSGameUtilsKt.getShipMountedTo((Entity)(Object)this);
+        if (ship == null)
+            return;
+
+        Matrix4d matrix = (Matrix4d) ship.getTransform().getShipToWorld();
+        double yaw = Math.toDegrees(Math.atan2(-matrix.getRow(0, new Vector3d()).z, matrix.getRow(2, new Vector3d()).z));
+        this.setYRot((float) (this.getYRot() + yaw) % 360);
+    }
+
+
+    //    Who called this method twice?????
+    @Inject(
+            method = "startRiding(Lnet/minecraft/world/entity/Entity;Z)Z",
+            at = @At("RETURN")
+    )
+    private void subShipYaw(Entity vehicle, boolean force, CallbackInfoReturnable<Boolean> cir) {
+        if(!level.isClientSide || !cir.getReturnValue())
+            return;
+
+        Ship ship = VSGameUtilsKt.getShipMountedTo((Entity)(Object)this);
+        if (ship == null)
+            return;
+
+        Matrix4d matrix = (Matrix4d) ship.getTransform().getShipToWorld();
+        double yaw = Math.toDegrees(Math.atan2(-matrix.getRow(0, new Vector3d()).z, matrix.getRow(2, new Vector3d()).z));
+        this.setYRot((float) (this.getYRot() - yaw) % 360);
+    }
+
+}
--- common/src/main/java/io/github/xiewuzhiying/vs_addition/mixin/xaeros_minimap/MixinMinimapRenderer.java
+++ common/src/main/java/io/github/xiewuzhiying/vs_addition/mixin/xaeros_minimap/MixinMinimapRenderer.java
@@ -1,30 +1,32 @@
 package io.github.xiewuzhiying.vs_addition.mixin.xaeros_minimap;
 
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import net.minecraft.world.entity.Entity;
+import net.minecraft.client.Minecraft;
 import org.joml.Matrix4d;
 import org.joml.Matrix4dc;
 import org.joml.Vector3d;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.valkyrienskies.core.api.ships.ClientShip;
-import org.valkyrienskies.mod.common.VSClientGameUtils;
 import org.valkyrienskies.mod.common.VSGameUtilsKt;
 import xaero.common.minimap.render.MinimapRenderer;
 
 @Mixin(MinimapRenderer.class)
 public abstract class MixinMinimapRenderer {
 
+    @Shadow protected Minecraft mc;
     @Unique
     private Matrix4dc latestMatrix4d = new Matrix4d();
 
     @Unique
     private double shipYawAngle = 0;
 
-    @WrapOperation(
+    @ModifyExpressionValue(
             method = "getRenderAngle",
             at = @At(
                     value = "INVOKE",
@@ -32,15 +34,15 @@ public abstract class MixinMinimapRenderer {
             ),
             remap = false
     )
-    public double getActualAngle(MinimapRenderer instance, Operation<Double> original) {
-        ClientShip ship = (ClientShip) VSGameUtilsKt.getShipMountedTo(((MinimapRendererAccessor) instance).getMinecraft().gameRenderer.getMainCamera().getEntity());
+    public double useActualAngle(double original) {
+        ClientShip ship = (ClientShip) VSGameUtilsKt.getShipMountedTo(mc.gameRenderer.getMainCamera().getEntity());
         if (ship != null) {
             Matrix4d matrix = ship.getRenderTransform().getShipToWorld().invert(new Matrix4d()).mul(latestMatrix4d);
             latestMatrix4d = ship.getRenderTransform().getShipToWorld();
             shipYawAngle = shipYawAngle + Math.toDegrees(Math.atan2(-matrix.getRow(0, new Vector3d()).z, matrix.getRow(2, new Vector3d()).z));
-            return original.call(instance) + shipYawAngle;
+            return original + shipYawAngle;
         } else {
-            return original.call(instance);
+            return original;
         }
     }
 }
