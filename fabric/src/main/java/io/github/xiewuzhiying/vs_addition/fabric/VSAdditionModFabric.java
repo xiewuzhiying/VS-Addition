e77d729fe677964d8682f75c386b77ac51d682c1
xiewuzhiying <HoyhCreate@outlook.com>
2024-07-15 23:58:41 +0800

    don't need create

--- fabric/src/main/java/io/github/xiewuzhiying/vs_addition/fabric/VSAdditionModFabric.java
+++ fabric/src/main/java/io/github/xiewuzhiying/vs_addition/fabric/VSAdditionModFabric.java
@@ -3,26 +3,20 @@ package io.github.xiewuzhiying.vs_addition.fabric;
 import dan200.computercraft.api.peripheral.PeripheralLookup;
 import io.github.xiewuzhiying.vs_addition.VSAdditionMod;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.loader.api.FabricLoader;
 import org.valkyrienskies.mod.fabric.common.ValkyrienSkiesModFabric;
 
 import static io.github.xiewuzhiying.vs_addition.VSAdditionMod.init;
 import static io.github.xiewuzhiying.vs_addition.fabric.compats.computercraft.FabricPeripheralLookup.peripheralProvider;
 
 public class VSAdditionModFabric implements ModInitializer {
-    private static  boolean VS2_ACTIVE = false;
-
     @Override
     public void onInitialize() {
         // force VS2 to load before eureka
-        VS2_ACTIVE = FabricLoader.getInstance().isModLoaded("valkyrienskies");
-
-        if(VS2_ACTIVE)
-            new ValkyrienSkiesModFabric().onInitialize();
+        new ValkyrienSkiesModFabric().onInitialize();
 
         init();
 
         if(VSAdditionMod.getCC_ACTIVE())
-            PeripheralLookup.get().registerFallback((level, blockPos, blockState, blockEntity, direction) -> peripheralProvider(level, blockPos));
+            PeripheralLookup.get().registerFallback((level, blockPos, blockState, blockEntity, direction) -> peripheralProvider(blockEntity));
     }
 }
