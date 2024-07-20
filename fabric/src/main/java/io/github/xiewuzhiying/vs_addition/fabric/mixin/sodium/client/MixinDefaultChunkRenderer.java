a79146f0b6f755a169109c329cd2bc8724737ba1
xiewuzhiying <HoyhCreate@outlook.com>
2024-07-21 01:55:34 +0800

    fix

--- fabric/src/main/java/io/github/xiewuzhiying/vs_addition/fabric/mixin/sodium/client/MixinDefaultChunkRenderer.java
+++ fabric/src/main/java/io/github/xiewuzhiying/vs_addition/fabric/mixin/sodium/client/MixinDefaultChunkRenderer.java
@@ -1,24 +1,27 @@
 package io.github.xiewuzhiying.vs_addition.fabric.mixin.sodium.client;
 
-import me.jellysquid.mods.sodium.client.model.quad.properties.ModelQuadFacing;
-import me.jellysquid.mods.sodium.client.render.chunk.DefaultChunkRenderer;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
+import me.jellysquid.mods.sodium.client.render.chunk.RegionChunkRenderer;
+import me.jellysquid.mods.sodium.client.render.chunk.data.ChunkRenderBounds;
 import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.valkyrienskies.mod.common.VSGameUtilsKt;
 
-@Mixin(DefaultChunkRenderer.class)
+@Mixin(RegionChunkRenderer.class)
 public class MixinDefaultChunkRenderer {
-    @Inject(
-            method = "getVisibleFaces",
-            at = @At("HEAD"),
-            cancellable = true,
+    @ModifyExpressionValue(
+            method = "buildDrawBatches",
+            at = @At(
+                    value = "FIELD",
+                    target = "Lme/jellysquid/mods/sodium/client/render/chunk/RegionChunkRenderer;isBlockFaceCullingEnabled:Z"
+            ),
             remap = false
     )
-    private static void cancelBlockFaceCulling(int originX, int originY, int originZ, int chunkX, int chunkY, int chunkZ, CallbackInfoReturnable<Integer> cir) {
-        if(VSGameUtilsKt.isChunkInShipyard(Minecraft.getInstance().level, chunkX, chunkZ))
-            cir.setReturnValue(ModelQuadFacing.ALL);
+    private boolean cancelBlockFaceCulling(boolean original, @Local ChunkRenderBounds bounds) {
+        if(VSGameUtilsKt.isBlockInShipyard(Minecraft.getInstance().level, bounds.x1, bounds.y1, bounds.z1))
+            return false;
+        return original;
     }
 }
