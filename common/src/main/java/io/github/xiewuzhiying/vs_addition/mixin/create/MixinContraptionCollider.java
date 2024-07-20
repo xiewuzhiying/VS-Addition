6080c00d2d0059443e9e25f038311dd2b0eb9a70
xiewuzhiying <HoyhCreate@outlook.com>
2024-07-20 23:35:38 +0800

    set lastShipStoopOn when entity collide with contraption on ship

---
+++ common/src/main/java/io/github/xiewuzhiying/vs_addition/mixin/create/MixinContraptionCollider.java
@@ -0,0 +1,29 @@
+package io.github.xiewuzhiying.vs_addition.mixin.create;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import com.simibubi.create.content.contraptions.AbstractContraptionEntity;
+import com.simibubi.create.content.contraptions.ContraptionCollider;
+import net.minecraft.world.entity.Entity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.valkyrienskies.core.api.ships.Ship;
+import org.valkyrienskies.mod.common.VSGameUtilsKt;
+import org.valkyrienskies.mod.common.util.IEntityDraggingInformationProvider;
+
+@Mixin(ContraptionCollider.class)
+public abstract class MixinContraptionCollider {
+    @Inject(
+            method = "collideEntities",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/entity/Entity;setDeltaMovement(Lnet/minecraft/world/phys/Vec3;)V"
+            )
+    )
+    private static void setLast(AbstractContraptionEntity contraptionEntity, CallbackInfo ci, @Local(ordinal = 0) Entity entity) {
+        Ship ship = VSGameUtilsKt.getShipManagingPos(contraptionEntity.level(), contraptionEntity.position());
+        if(ship != null)
+            ((IEntityDraggingInformationProvider)entity).getDraggingInformation().setLastShipStoodOn(ship.getId());
+    }
+}
