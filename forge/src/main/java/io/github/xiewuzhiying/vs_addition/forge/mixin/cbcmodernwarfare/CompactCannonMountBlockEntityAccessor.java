2e7868d1d7638b07a056d4199a277ef7e1342582
xiewuzhiying <HoyhCreate@outlook.com>
2024-07-22 14:24:09 +0800

    Port modern warfare compat to 1.20.1

--- forge/src/main/java/io/github/xiewuzhiying/vs_addition/forge/mixin/cbcmodernwarfare/CompactCannonMountBlockEntityAccessor.java
+++ forge/src/main/java/io/github/xiewuzhiying/vs_addition/forge/mixin/cbcmodernwarfare/CompactCannonMountBlockEntityAccessor.java
@@ -1,18 +1,25 @@
-//package io.github.xiewuzhiying.vs_addition.forge.mixin.cbcmodernwarfare;
-//
-//import org.spongepowered.asm.mixin.Mixin;
-//import org.spongepowered.asm.mixin.gen.Invoker;
-//import riftyboi.cbcmodernwarfare.cannon_control.compact_mount.CompactCannonMountBlockEntity;
-//
-//@Mixin(CompactCannonMountBlockEntity.class)
-//public interface CompactCannonMountBlockEntityAccessor {
-//
-//    @Invoker(value = "assemble", remap = false)
-//    void Assemble();
-//
-//    @Invoker(value = "getMaxDepress", remap = false)
-//    float GetMaxDepress();
-//
-//    @Invoker(value = "getMaxElevate", remap = false)
-//    float GetMaxElevate();
-//}
+package io.github.xiewuzhiying.vs_addition.forge.mixin.cbcmodernwarfare;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.gen.Invoker;
+import riftyboi.cbcmodernwarfare.cannon_control.compact_mount.CompactCannonMountBlockEntity;
+
+@Mixin(CompactCannonMountBlockEntity.class)
+public interface CompactCannonMountBlockEntityAccessor {
+
+    @Invoker(value = "assemble", remap = false)
+    void Assemble();
+
+    @Invoker(value = "getMaxDepress", remap = false)
+    float GetMaxDepress();
+
+    @Invoker(value = "getMaxElevate", remap = false)
+    float GetMaxElevate();
+
+    @Accessor(remap = false)
+    float getCannonYaw();
+
+    @Accessor(remap = false)
+    float getCannonPitch();
+}
