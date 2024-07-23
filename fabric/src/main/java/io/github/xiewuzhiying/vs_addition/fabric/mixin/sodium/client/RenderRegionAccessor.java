b13ebcf7514abe7f16de3a0f989cab3c70ee5ffa
xiewuzhiying <HoyhCreate@outlook.com>
2024-07-23 12:18:48 +0800

    this maybe better

---
+++ fabric/src/main/java/io/github/xiewuzhiying/vs_addition/fabric/mixin/sodium/client/RenderRegionAccessor.java
@@ -0,0 +1,17 @@
+package io.github.xiewuzhiying.vs_addition.fabric.mixin.sodium.client;
+
+import me.jellysquid.mods.sodium.client.render.chunk.region.RenderRegion;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(value = RenderRegion.class, remap = false)
+public interface RenderRegionAccessor {
+    @Accessor("x")
+    int getX();
+
+    @Accessor("y")
+    int getY();
+
+    @Accessor("z")
+    int getZ();
+}
