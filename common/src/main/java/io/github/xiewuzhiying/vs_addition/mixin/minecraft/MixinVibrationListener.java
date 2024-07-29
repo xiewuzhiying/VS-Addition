bc746bfee26f2fb0315bb1239ee5bf072a9e3fe9
xiewuzhiying <HoyhCreate@outlook.com>
2024-07-29 23:30:07 +0800

    like this?

--- common/src/main/java/io/github/xiewuzhiying/vs_addition/mixin/minecraft/MixinVibrationListener.java
+++ common/src/main/java/io/github/xiewuzhiying/vs_addition/mixin/minecraft/MixinVibrationListener.java
@@ -4,11 +4,18 @@ import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import java.util.Optional;
+
+import com.llamalad7.mixinextras.sugar.Share;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
+import io.github.xiewuzhiying.vs_addition.util.RaycastUtils;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.ClipBlockStateContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.level.gameevent.GameEvent.Context;
 import net.minecraft.world.level.gameevent.vibrations.VibrationSystem.User;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -26,11 +33,22 @@ public abstract class MixinVibrationListener {
             )
     )
     private boolean betweenInclShips(final User instance, final ServerLevel serverLevel, final BlockPos blockPos, final GameEvent gameEvent, final Context context,
-                        final Operation<Boolean> original, @Local(argsOnly = true) final Vec3 vec3) {
+                                     final Operation<Boolean> original, @Local(argsOnly = true) final Vec3 arg4, final @Share("newArg4") LocalRef<Vec3> newArg4) {
         final Optional<Vec3> userVec3 = instance.getPositionSource().getPosition(serverLevel);
         final Ship userShip = userVec3.map(value -> VSGameUtilsKt.getShipManagingPos(serverLevel, value)).orElse(null);
-        final Vec3 newVec3 = VSGameUtilsKt.toWorldCoordinates(serverLevel, vec3);
-        final BlockPos newPos = BlockPos.containing(userShip != null ? VectorConversionsMCKt.toMinecraft(userShip.getWorldToShip().transformPosition(VectorConversionsMCKt.toJOML(newVec3))) : newVec3);
+        newArg4.set(VSGameUtilsKt.toWorldCoordinates(serverLevel, arg4));
+        final BlockPos newPos = BlockPos.containing(userShip != null ? VectorConversionsMCKt.toMinecraft(userShip.getWorldToShip().transformPosition(VectorConversionsMCKt.toJOML(newArg4.get()))) : newArg4.get());
         return original.call(instance, serverLevel, newPos, gameEvent, context);
+    }
+
+    @WrapOperation(
+            method = "isOccluded",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/level/Level;isBlockInLine(Lnet/minecraft/world/level/ClipBlockStateContext;)Lnet/minecraft/world/phys/BlockHitResult;"
+            )
+    )
+    private static BlockHitResult man(Level instance, ClipBlockStateContext clipBlockStateContext, Operation<BlockHitResult> original) {
+        return RaycastUtils.isBlockInLineIncludeShips(instance, clipBlockStateContext);
     }
 }
