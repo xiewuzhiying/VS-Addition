40732eb86519eec864e032249ed56c96596e1f6b
xiewuzhiying <HoyhCreate@outlook.com>
2024-07-30 02:44:29 +0800

    try to fix phys bearing

---
+++ common/src/main/java/io/github/xiewuzhiying/vs_addition/mixinducks/vs_clockwork/phys_bearing/UpdateIsFacingNegativeDirection.java
@@ -0,0 +1,5 @@
+package io.github.xiewuzhiying.vs_addition.mixinducks.vs_clockwork.phys_bearing;
+
+public interface UpdateIsFacingNegativeDirection {
+    default void vs_addition$updateIsFacingNegativeDirection(boolean bl) {}
+}
