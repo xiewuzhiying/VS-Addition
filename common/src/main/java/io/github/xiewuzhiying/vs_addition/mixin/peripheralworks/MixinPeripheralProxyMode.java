c8aaa8b7ae1136cce94b80434d16a754ce545fbf
xiewuzhiying <HoyhCreate@outlook.com>
2024-07-29 23:30:00 +0800

    was wrong

---
+++ common/src/main/java/io/github/xiewuzhiying/vs_addition/mixin/peripheralworks/MixinPeripheralProxyMode.java
@@ -0,0 +1,33 @@
+package io.github.xiewuzhiying.vs_addition.mixin.peripheralworks;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.core.BlockPos;
+import net.minecraft.util.Mth;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.valkyrienskies.mod.common.VSGameUtilsKt;
+import site.siredvin.peripheralworks.common.blockentity.PeripheralProxyBlockEntity;
+import site.siredvin.peripheralworks.common.configuration.PeripheralWorksConfig;
+import site.siredvin.peripheralworks.subsystem.configurator.PeripheralProxyMode;
+
+@Mixin(PeripheralProxyMode.class)
+public abstract class MixinPeripheralProxyMode {
+
+    @WrapOperation(
+            method = "onBlockClick",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lsite/siredvin/peripheralworks/common/blockentity/PeripheralProxyBlockEntity;isPosApplicable(Lnet/minecraft/core/BlockPos;)Z"
+            )
+    )
+    private boolean betweenInclShips(final PeripheralProxyBlockEntity instance, final BlockPos pos, final Operation<Boolean> original, final @Local(argsOnly = true) Level level) {
+        final BlockPos instancePos = instance.getBlockPos();
+        if (pos == instancePos) {
+            return false;
+        }
+        return VSGameUtilsKt.squaredDistanceBetweenInclShips(level, instancePos.getX(), instancePos.getY(), instancePos.getZ(), pos.getX(), pos.getY(), pos.getZ()) < Mth.square(PeripheralWorksConfig.INSTANCE.getPeripheralProxyMaxRange());
+    }
+}
