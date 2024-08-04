ef3b246f70f7c1ecff3a142ffaf390ea61b5a0fd
xiewuzhiying <HoyhCreate@outlook.com>
2024-08-04 09:54:36 +0800

    minor fix

--- common/src/main/java/io/github/xiewuzhiying/vs_addition/mixin/create/fan/MixinNozzleBlockEntity.java
+++ common/src/main/java/io/github/xiewuzhiying/vs_addition/mixin/create/fan/MixinNozzleBlockEntity.java
@@ -1,8 +1,9 @@
 package io.github.xiewuzhiying.vs_addition.mixin.create.fan;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.simibubi.create.content.kinetics.fan.NozzleBlockEntity;
 import com.simibubi.create.foundation.blockEntity.SmartBlockEntity;
-import com.simibubi.create.foundation.utility.VecHelper;
 import io.github.xiewuzhiying.vs_addition.util.TransformUtils;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Vec3i;
@@ -12,7 +13,6 @@ import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Pseudo
 @Mixin(NozzleBlockEntity.class)
@@ -20,8 +20,8 @@ public abstract class MixinNozzleBlockEntity extends SmartBlockEntity {
     public MixinNozzleBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state) {
         super(type, pos, state);
     }
-    @Redirect(method = {"tick","lazyTick","canSee"},at = @At(value = "INVOKE", target = "Lcom/simibubi/create/foundation/utility/VecHelper;getCenterOf(Lnet/minecraft/core/Vec3i;)Lnet/minecraft/world/phys/Vec3;"),remap = false)
-    public Vec3 transformToWorldPos(Vec3i pos){
-        return TransformUtils.toWorldVec3(this.level,VecHelper.getCenterOf(pos));
+    @WrapOperation(method = {"tick","lazyTick","canSee"},at = @At(value = "INVOKE", target = "Lcom/simibubi/create/foundation/utility/VecHelper;getCenterOf(Lnet/minecraft/core/Vec3i;)Lnet/minecraft/world/phys/Vec3;"))
+    private Vec3 transformToWorldPos(Vec3i pos, Operation<Vec3> original){
+        return TransformUtils.toWorldVec3(this.level,TransformUtils.getCenterOf(pos));
     }
 }
