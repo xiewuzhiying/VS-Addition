e875b0dd3ab74138b2b76642c2c71f8f362bbc2f
xiewuzhiying <HoyhCreate@outlook.com>
2024-08-06 02:36:01 +0800

    I'm a fool

--- forge/src/main/java/io/github/xiewuzhiying/vs_addition/forge/mixin/VSAdditionForgeMixinCanceller.java
+++ forge/src/main/java/io/github/xiewuzhiying/vs_addition/forge/mixin/VSAdditionForgeMixinCanceller.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class VSAdditionForgeMixinCanceller implements MixinCanceller {
     @Override
     public boolean shouldCancel(List<String> targetClassNames, String mixinClassName) {
-        if(mixinClassName.contains("org.valkyrienskies.mod.fabric.mixin.compat.cc_tweaked.MixinSpeakerPosition"))
+        if(mixinClassName.contains("org.valkyrienskies.mod.forge.mixin.compat.cc_tweaked.MixinSpeakerPosition"))
             return true;
         return false;
     }
