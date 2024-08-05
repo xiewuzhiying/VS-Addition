81024defaa29bbf8442c87b0c136ca109f1b2396
xiewuzhiying <HoyhCreate@outlook.com>
2024-08-05 08:20:10 +0800

    shut up

--- common/src/main/java/io/github/xiewuzhiying/vs_addition/mixin/valkyrienskies/disableLogs/MixinAl.java
+++ common/src/main/java/io/github/xiewuzhiying/vs_addition/mixin/valkyrienskies/disableLogs/MixinAl.java
@@ -1,26 +1,37 @@
 package io.github.xiewuzhiying.vs_addition.mixin.valkyrienskies.disableLogs;
 
-
 import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import io.github.xiewuzhiying.vs_addition.VSAdditionConfig;
 import org.apache.logging.log4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
-import org.valkyrienskies.core.impl.game.ships.ShipObjectServerWorld;
+import org.valkyrienskies.core.impl.shadow.Al;
 
 @Pseudo
-@Mixin(ShipObjectServerWorld.class)
-public abstract class MixinShipObjectServerWorld {
+@Mixin(Al.class)
+public abstract class MixinAl {
     @WrapWithCondition(
-            method = "postTick",
+            method = "b(Lorg/valkyrienskies/core/impl/shadow/Ap;)V",
             at = @At(
                     value = "INVOKE",
                     target = "Lorg/apache/logging/log4j/Logger;warn(Ljava/lang/String;)V"
             ),
             remap = false
     )
-    private boolean cancelLogging(Logger instance, String string) {
+    private boolean cancelLogging1(Logger instance, String s) {
+        return !VSAdditionConfig.SERVER.getDisableSomeWarnings();
+    }
+
+    @WrapWithCondition(
+            method = "b(Lorg/valkyrienskies/core/impl/shadow/Aw;)V",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lorg/apache/logging/log4j/Logger;warn(Ljava/lang/String;)V"
+            ),
+            remap = false
+    )
+    private boolean cancelLogging2(Logger instance, String s) {
         return !VSAdditionConfig.SERVER.getDisableSomeWarnings();
     }
 }
