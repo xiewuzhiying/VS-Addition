279e3fc8af8ce136d6a2d3a4543b05d4432c00b5
xiewuzhiying <HoyhCreate@outlook.com>
2024-08-05 17:11:21 +0800

    minor fix

--- common/src/main/java/io/github/xiewuzhiying/vs_addition/mixin/valkyrienskies/disableLogs/MixinAy.java
+++ common/src/main/java/io/github/xiewuzhiying/vs_addition/mixin/valkyrienskies/disableLogs/MixinAy.java
@@ -17,6 +17,7 @@ public abstract class MixinAy {
                     value = "INVOKE",
                     target = "Lorg/apache/logging/log4j/Logger;warn(Ljava/lang/String;)V"
             ),
+            require = 0,
             remap = false
     )
     private boolean cancelLogging1(Logger instance, String s) {
