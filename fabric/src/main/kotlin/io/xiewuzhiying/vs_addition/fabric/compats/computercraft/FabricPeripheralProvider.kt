add21c4142c9659ee310dc4ac3725e1b044643fd
xiewuzhiying <HoyhCreate@outlook.com>
2024-08-05 12:58:05 +0800

    em

--- fabric/src/main/kotlin/io/xiewuzhiying/vs_addition/fabric/compats/computercraft/FabricPeripheralProvider.kt
+++ fabric/src/main/kotlin/io/xiewuzhiying/vs_addition/fabric/compats/computercraft/FabricPeripheralProvider.kt
@@ -2,7 +2,7 @@ package io.xiewuzhiying.vs_addition.fabric.compats.computercraft
 
 import dan200.computercraft.api.peripheral.IPeripheral
 import dan200.computercraft.api.peripheral.IPeripheralProvider
-import io.github.xiewuzhiying.vs_addition.compats.computercraft.PeripheralCommon.getPeripheralCommon
+import io.github.xiewuzhiying.vs_addition.compats.computercraft.PeripheralCommon
 import net.minecraft.core.BlockPos
 import net.minecraft.core.Direction
 import net.minecraft.world.level.Level
@@ -10,6 +10,6 @@ import net.minecraft.world.level.Level
 class FabricPeripheralProvider : IPeripheralProvider {
     override fun getPeripheral(level: Level, blockPos: BlockPos,direction: Direction
     ): IPeripheral? {
-        return getPeripheralCommon(level, blockPos)
+        return PeripheralCommon.getPeripheralCommon(level, blockPos)
     }
 }
