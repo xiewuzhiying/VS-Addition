a75f0f2e09d628337b8a442aee360121d4aa7b28
xiewuzhiying <HoyhCreate@outlook.com>
2024-08-05 16:15:19 +0800

    minor fix

--- forge/src/main/java/io/github/xiewuzhiying/vs_addition/forge/mixin/presencefootsteps/client/MixinTerrestrialStepSoundGenerator.java
+++ forge/src/main/java/io/github/xiewuzhiying/vs_addition/forge/mixin/presencefootsteps/client/MixinTerrestrialStepSoundGenerator.java
@@ -2,7 +2,6 @@ package io.github.xiewuzhiying.vs_addition.forge.mixin.presencefootsteps.client;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import com.llamalad7.mixinextras.sugar.Local;
 import eu.ha3.presencefootsteps.world.Association;
 import eu.ha3.presencefootsteps.world.Solver;
 import net.minecraft.core.BlockPos;
@@ -22,10 +21,10 @@ public abstract class MixinTerrestrialStepSoundGenerator {
             method = "Leu/ha3/presencefootsteps/sound/generator/TerrestrialStepSoundGenerator;produceStep(Lnet/minecraft/world/entity/LivingEntity;Leu/ha3/presencefootsteps/sound/State;D)V",
             at = @At(
                     value = "INVOKE",
-                    target = "Leu/ha3/presencefootsteps/world/Solver;findAssociation(Lnet/minecraft/core/BlockPos;Ljava/lang/String;)Leu/ha3/presencefootsteps/world/Association;"
+                    target = "Leu/ha3/presencefootsteps/world/Solver;findAssociation(Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/core/BlockPos;Ljava/lang/String;)Leu/ha3/presencefootsteps/world/Association;"
             )
     )
-    private Association includeShips1(Solver instance, BlockPos pos, String strategy, Operation<Association> original, @Local(argsOnly = true) LivingEntity entity) {
+    private Association includeShips1(Solver instance, LivingEntity entity, BlockPos pos, String strategy, Operation<Association> original) {
         Vec3 vec3 = new Vec3(entity.getX(), entity.getBoundingBox().minY -0.001, entity.getZ());
         BlockHitResult result = RaycastUtilsKt.clipIncludeShips(entity.level(), new ClipContext(
                 vec3,
@@ -34,17 +33,17 @@ public abstract class MixinTerrestrialStepSoundGenerator {
                 ClipContext.Fluid.NONE,
                 null
         ));
-        return original.call(instance, result.getBlockPos(), strategy);
+        return original.call(instance, entity, result.getBlockPos(), strategy);
     }
 
     @WrapOperation(
             method = "produceStep(Lnet/minecraft/world/entity/LivingEntity;Leu/ha3/presencefootsteps/sound/State;D)V",
             at = @At(
                     value = "INVOKE",
-                    target = "Leu/ha3/presencefootsteps/world/Solver;findAssociation(Lnet/minecraft/core/BlockPos;Ljava/lang/String;)Leu/ha3/presencefootsteps/world/Association;"
+                    target = "Leu/ha3/presencefootsteps/world/Solver;findAssociation(Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/core/BlockPos;Ljava/lang/String;)Leu/ha3/presencefootsteps/world/Association;"
             )
     )
-    private Association includeShips2(Solver instance, BlockPos pos, String strategy, Operation<Association> original, @Local(argsOnly = true) LivingEntity entity) {
+    private Association includeShips2(Solver instance, LivingEntity entity, BlockPos pos, String strategy, Operation<Association> original) {
         Vec3 vec3 = new Vec3(entity.getX(), entity.getBoundingBox().minY -0.001, entity.getZ());
         BlockHitResult result = RaycastUtilsKt.clipIncludeShips(entity.level(), new ClipContext(
                 vec3,
@@ -53,6 +52,6 @@ public abstract class MixinTerrestrialStepSoundGenerator {
                 ClipContext.Fluid.NONE,
                 null
         ));
-        return original.call(instance, result.getBlockPos(), strategy);
+        return original.call(instance, entity, result.getBlockPos(), strategy);
     }
 }
