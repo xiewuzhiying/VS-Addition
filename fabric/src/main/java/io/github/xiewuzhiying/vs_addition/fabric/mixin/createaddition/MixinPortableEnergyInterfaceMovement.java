c3af3301541c038690d4d82998fe3ec29b9b7058
xiewuzhiying <HoyhCreate@outlook.com>
2024-08-09 23:58:02 +0800

    Fixed confusing PSI mode

--- fabric/src/main/java/io/github/xiewuzhiying/vs_addition/fabric/mixin/createaddition/MixinPortableEnergyInterfaceMovement.java
+++ fabric/src/main/java/io/github/xiewuzhiying/vs_addition/fabric/mixin/createaddition/MixinPortableEnergyInterfaceMovement.java
@@ -1,6 +1,8 @@
 package io.github.xiewuzhiying.vs_addition.fabric.mixin.createaddition;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import com.mrh0.createaddition.blocks.portable_energy_interface.PortableEnergyInterfaceBlock;
 import com.mrh0.createaddition.blocks.portable_energy_interface.PortableEnergyInterfaceBlockEntity;
@@ -9,6 +11,7 @@ import com.simibubi.create.content.contraptions.actors.psi.PortableStorageInterf
 import com.simibubi.create.content.contraptions.behaviour.MovementBehaviour;
 import com.simibubi.create.content.contraptions.behaviour.MovementContext;
 import com.simibubi.create.foundation.utility.VecHelper;
+import io.github.xiewuzhiying.vs_addition.mixinducks.create.portable_interface.IPSIBehavior;
 import io.github.xiewuzhiying.vs_addition.util.TransformUtilsKt;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
@@ -21,7 +24,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.valkyrienskies.core.api.ships.Ship;
 import org.valkyrienskies.mod.common.VSGameUtilsKt;
 import org.valkyrienskies.mod.common.util.VectorConversionsMCKt;
@@ -39,115 +41,144 @@ public abstract class MixinPortableEnergyInterfaceMovement implements MovementBe
                     target = "Lcom/simibubi/create/foundation/utility/VecHelper;getCenterOf(Lnet/minecraft/core/Vec3i;)Lnet/minecraft/world/phys/Vec3;"
             )
     )
-    private Vec3 getCenterOf(Vec3 original, @Local MovementContext context) {
-        Vec3 transfromedPos = VSGameUtilsKt.toWorldCoordinates(context.world, original);
-        Ship ship = VSGameUtilsKt.getShipManagingPos(context.world, context.position);
-        if(ship!=null)
-            transfromedPos = VectorConversionsMCKt.toMinecraft(ship.getTransform().getWorldToShip().transformPosition(VectorConversionsMCKt.toJOML(transfromedPos)));
-        return transfromedPos;
+    private Vec3 getCenterOf(Vec3 original, @Local(argsOnly = true) MovementContext context) {
+        if(context.blockEntityData.getInt("ScrollValue") == 1) {
+            Vec3 transfromedPos = VSGameUtilsKt.toWorldCoordinates(context.world, original);
+            Ship ship = VSGameUtilsKt.getShipManagingPos(context.world, context.position);
+            if (ship != null)
+                transfromedPos = VectorConversionsMCKt.toMinecraft(ship.getTransform().getWorldToShip().transformPosition(VectorConversionsMCKt.toJOML(transfromedPos)));
+            return transfromedPos;
+        } else {
+            return original;
+        }
     }
 
-    @Redirect(
+    @WrapOperation(
             method = "findInterface",
             at = @At(
                     value = "INVOKE",
                     target = "Lcom/mrh0/createaddition/blocks/portable_energy_interface/PortableEnergyInterfaceMovement;findStationaryInterface(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/Direction;)Lcom/mrh0/createaddition/blocks/portable_energy_interface/PortableEnergyInterfaceBlockEntity;"
             )
     )
-    public PortableEnergyInterfaceBlockEntity findStationaryInterface(PortableEnergyInterfaceMovement instance, Level level, BlockPos blockPos, BlockState world, Direction pos, @Local MovementContext context) {
-        Ship selfShip = VSGameUtilsKt.getShipManagingPos(level, blockPos);
-        Vector3d selfDirectionVec = VectorConversionsMCKt.toJOML(context.rotation.apply(Vec3.atLowerCornerOf(context.state
-                .getValue(PortableEnergyInterfaceBlock.FACING).getNormal())));
+    public PortableEnergyInterfaceBlockEntity findStationaryInterface(PortableEnergyInterfaceMovement instance, Level level, BlockPos blockPos, BlockState state, Direction direction, Operation<PortableEnergyInterfaceBlockEntity> original, @Local(argsOnly = true) MovementContext context) {
+        if(context.blockEntityData.getInt("ScrollValue") == 1) {
+            Ship selfShip = VSGameUtilsKt.getShipManagingPos(level, blockPos);
+            Vector3d selfDirectionVec = VectorConversionsMCKt.toJOML(context.rotation.apply(Vec3.atLowerCornerOf(context.state
+                    .getValue(PortableEnergyInterfaceBlock.FACING).getNormal())));
 
-        for (int i = 0; i < 2; i++) {
+            for (int i = 0; i < 2; i++) {
 
-            Vector3d checkPos = VSGameUtilsKt.toWorldCoordinates(level, new Vector3d(blockPos.getX(), blockPos.getY(), blockPos.getZ()).add(0.5, 0.5, 0.5));
-            if (i!=0){
-                checkPos = checkPos.add(selfDirectionVec.mul(i-0.125));
-            }
-            List<Vector3d> ships = VSGameUtilsKt.transformToNearbyShipsAndWorld(level, checkPos.x, checkPos.y, checkPos.z, 2);
-            ships.add(VSGameUtilsKt.toWorldCoordinates(level, checkPos));
+                Vector3d checkPos = VSGameUtilsKt.toWorldCoordinates(level, new Vector3d(blockPos.getX(), blockPos.getY(), blockPos.getZ()).add(0.5, 0.5, 0.5));
+                if (i != 0) {
+                    checkPos = checkPos.add(selfDirectionVec.mul(i - 0.125));
+                }
+                List<Vector3d> ships = VSGameUtilsKt.transformToNearbyShipsAndWorld(level, checkPos.x, checkPos.y, checkPos.z, 2);
+                ships.add(VSGameUtilsKt.toWorldCoordinates(level, checkPos));
 
-            for(Vector3d eachShipPos : ships) {
-                PortableEnergyInterfaceBlockEntity psi = findPSI(level, eachShipPos);
-                if (psi != null) {
-                    Vector3d selfVec = selfDirectionVec.normalize().negate();
-                    if (selfShip!=null)
-                        selfVec = selfVec.rotate(selfShip.getTransform().getShipToWorldRotation());
-                    Vector3d directionVec = VectorConversionsMCKt.toJOML(Vec3.atLowerCornerOf(psi.getBlockState()
-                            .getValue(PortableStorageInterfaceBlock.FACING).getNormal())).normalize();
-                    Ship ship = VSGameUtilsKt.getShipManagingPos(level, eachShipPos);
-                    if (ship!=null)
-                        directionVec = directionVec.rotate(ship.getTransform().getShipToWorldRotation());
-                    if (Math.toDegrees(Math.acos(new Vector3d(directionVec).dot(new Vector3d(selfVec))))  <= 22.5)
-                        return psi;
+                for (Vector3d eachShipPos : ships) {
+                    PortableEnergyInterfaceBlockEntity psi = vs_addition$findPSI(level, eachShipPos);
+                    if (psi != null) {
+                        Vector3d selfVec = selfDirectionVec.normalize().negate();
+                        if (selfShip != null)
+                            selfVec = selfVec.rotate(selfShip.getTransform().getShipToWorldRotation());
+                        Vector3d directionVec = VectorConversionsMCKt.toJOML(Vec3.atLowerCornerOf(psi.getBlockState()
+                                .getValue(PortableStorageInterfaceBlock.FACING).getNormal())).normalize();
+                        Ship ship = VSGameUtilsKt.getShipManagingPos(level, eachShipPos);
+                        if (ship != null)
+                            directionVec = directionVec.rotate(ship.getTransform().getShipToWorldRotation());
+                        if (Math.toDegrees(Math.acos(new Vector3d(directionVec).dot(new Vector3d(selfVec)))) <= 22.5)
+                            return psi;
+                    }
                 }
             }
+            return null;
+        } else {
+            return original.call(instance, level, blockPos, state, direction);
         }
-        return null;
     }
 
-    @Redirect(
+    @WrapOperation(
             method = "tick",
             at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/world/phys/Vec3;closerThan(Lnet/minecraft/core/Position;D)Z"
             )
     )
-    public boolean closerThan(Vec3 vec, Position pos, double distance, @Local MovementContext context) {
-        return VSGameUtilsKt.squaredDistanceBetweenInclShips(context.world, vec.x, vec.y, vec.z, pos.x(), pos.y(), pos.z()) < distance * distance;
+    public boolean closerThan(Vec3 vec, Position pos, double distance, Operation<Boolean> original, @Local(argsOnly = true) MovementContext context) {
+        if(context.blockEntityData.getInt("ScrollValue") == 1) {
+            return VSGameUtilsKt.squaredDistanceBetweenInclShips(context.world, vec.x, vec.y, vec.z, pos.x(), pos.y(), pos.z()) < distance * distance;
+        } else {
+            return original.call(vec, pos, distance);
+        }
     }
 
-    @Redirect(
+    @WrapOperation(
             method = "tick",
             at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/world/phys/Vec3;distanceTo(Lnet/minecraft/world/phys/Vec3;)D"
             )
     )
-    public double distanceTo(Vec3 vec1, Vec3 vec2, @Local MovementContext context) {
-        return VSGameUtilsKt.squaredDistanceBetweenInclShips(context.world, vec1.x, vec1.y, vec1.z, vec2.x(), vec2.y(), vec2.z());
+    public double distanceTo(Vec3 vec31, Vec3 vec32, Operation<Double> original, @Local(argsOnly = true) MovementContext context) {
+        if(context.blockEntityData.getInt("ScrollValue") == 1) {
+            return VSGameUtilsKt.squaredDistanceBetweenInclShips(context.world, vec31.x, vec31.y, vec31.z, vec32.x(), vec32.y(), vec32.z());
+        } else {
+            return original.call(vec31, vec32);
+        }
     }
 
-    @Redirect(
+    @WrapOperation(
             method = "tick",
             at = @At(
                     value = "INVOKE",
                     target = "Lcom/mrh0/createaddition/blocks/portable_energy_interface/PortableEnergyInterfaceMovement;getStationaryInterfaceAt(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/Direction;)Lcom/mrh0/createaddition/blocks/portable_energy_interface/PortableEnergyInterfaceBlockEntity;"
-            )
+            ),
+            remap = false
     )
-    public PortableEnergyInterfaceBlockEntity redirectToFindPSI(PortableEnergyInterfaceMovement instance, Level level, BlockPos pos, BlockState state, Direction direction) {
-        return findPSI(level, VectorConversionsMCKt.toJOML(VecHelper.getCenterOf(pos)));
+    public PortableEnergyInterfaceBlockEntity redirectToFindPSI(PortableEnergyInterfaceMovement instance, Level level, BlockPos pos, BlockState state, Direction direction, Operation<PortableEnergyInterfaceBlockEntity> original, @Local(ordinal = 0, argsOnly = true) MovementContext context) {
+        if(context.blockEntityData.getInt("ScrollValue") == 1) {
+            return vs_addition$findPSI(level, VectorConversionsMCKt.toJOML(VecHelper.getCenterOf(pos)));
+        } else {
+            return original.call(instance, level, pos, state, direction);
+        }
     }
 
-    @Redirect(
+    @ModifyExpressionValue(
             method = "tick",
             at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/world/phys/Vec3;length()D"
             )
     )
-    public double modfiyLength(Vec3 instance) {
-        return 0;
+    public double modfiyLength(double original, @Local(ordinal = 0, argsOnly = true) MovementContext context) {
+        if(context.blockEntityData.getInt("ScrollValue") == 1) {
+            return 0;
+        } else {
+            return original;
+        }
     }
 
-    @Redirect(
+    @ModifyExpressionValue(
             method = "visitNewPosition",
             at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/world/phys/Vec3;length()D"
             )
     )
-    public double modfiyLength2(Vec3 instance) {
-        return 0;
+    public double modfiyLength2(double original, @Local(ordinal = 0, argsOnly = true) MovementContext context) {
+        if(context.blockEntityData.getInt("ScrollValue") == 1) {
+            return 0;
+        } else {
+            return original;
+        }
     }
 
 
     @Unique
-    public PortableEnergyInterfaceBlockEntity findPSI(Level level, Vector3d pos) {
+    public PortableEnergyInterfaceBlockEntity vs_addition$findPSI(Level level, Vector3d pos) {
         BlockPos checkThis = new BlockPos(TransformUtilsKt.getToBlockPos(pos));
         if(level.getBlockEntity(checkThis) instanceof PortableEnergyInterfaceBlockEntity psi) {
-            if(psi.isPowered())
+            if(psi.isPowered() || ((IPSIBehavior)psi).vs_addition$getWorkingMode().get() == IPSIBehavior.WorkigMode.ORIGINAL)
                 return null;
             return psi;
         }
