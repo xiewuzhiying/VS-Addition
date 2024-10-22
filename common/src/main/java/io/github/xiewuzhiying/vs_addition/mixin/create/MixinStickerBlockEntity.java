450f418ef9efa158f9c4b814ae4d75522baaf923
xiewuzhiying <HoyhCreate@outlook.com>
2024-10-23 03:22:37 +0800

    revert sticker

--- common/src/main/java/io/github/xiewuzhiying/vs_addition/mixin/create/MixinStickerBlockEntity.java
+++ common/src/main/java/io/github/xiewuzhiying/vs_addition/mixin/create/MixinStickerBlockEntity.java
@@ -1,13 +1,22 @@
 package io.github.xiewuzhiying.vs_addition.mixin.create;
 
+import com.simibubi.create.content.contraptions.chassis.StickerBlock;
 import com.simibubi.create.content.contraptions.chassis.StickerBlockEntity;
+import com.simibubi.create.content.contraptions.glue.SuperGlueItem;
 import com.simibubi.create.foundation.blockEntity.SmartBlockEntity;
-import io.github.xiewuzhiying.vs_addition.compats.create.content.contraptions.chassis.StickerWithShipHandler;
+import io.github.xiewuzhiying.vs_addition.VSAdditionConfig;
+import io.github.xiewuzhiying.vs_addition.util.TransformUtilsKt;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.Fluids;
+import org.joml.Quaterniond;
+import org.joml.Quaterniondc;
+import org.joml.Vector3d;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.Shadow;
@@ -15,26 +24,41 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.valkyrienskies.core.api.ships.ServerShip;
+import org.valkyrienskies.core.apigame.constraints.VSAttachmentConstraint;
+import org.valkyrienskies.core.apigame.constraints.VSFixedOrientationConstraint;
+import org.valkyrienskies.core.apigame.world.ServerShipWorldCore;
+import org.valkyrienskies.mod.common.VSGameUtilsKt;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static net.minecraft.world.level.block.state.properties.BlockStateProperties.FACING;
 
 @Pseudo
 @Mixin(StickerBlockEntity.class)
 public abstract class MixinStickerBlockEntity extends SmartBlockEntity {
 
-    @Shadow public abstract boolean isBlockStateExtended();
+    @Shadow(remap = false) public abstract boolean isBlockStateExtended();
 
     @Unique
-    private StickerWithShipHandler vs_addition$handler = null;
+    private final Map<BlockPos, List<Integer>> map = new HashMap<>();
+
+    @Unique
+    private final double compliance = VSAdditionConfig.SERVER.getCreate().getStickerCompliance();
+    @Unique
+    private final double maxForce = VSAdditionConfig.SERVER.getCreate().getStickerMaxForce();
+
+    @Unique
+    private boolean needUpdate = false;
+
+    @Unique
+    private boolean wasBlockStateExtended = false;
 
     public MixinStickerBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state) {
         super(type, pos, state);
-    }
-
-    @Inject(
-            method = "<init>",
-            at = @At("TAIL")
-    )
-    private void init(BlockEntityType<? extends StickerBlockEntity> type, BlockPos pos, BlockState state, CallbackInfo ci) {
-        this.vs_addition$handler = new StickerWithShipHandler(this.level, this.getBlockPos(), this::vs_addition$playClientSound);
     }
 
     @Inject(
@@ -45,30 +69,138 @@ public abstract class MixinStickerBlockEntity extends SmartBlockEntity {
                     shift = At.Shift.AFTER
             )
     )
-    private void tick(CallbackInfo ci) {
-        this.vs_addition$handler.tick(isBlockStateExtended());
+    public void stickerConstraints(CallbackInfo ci) {
+
+        if(!level.isClientSide && isBlockStateExtended()) {
+            ServerShipWorldCore serverShipWorldCore = VSGameUtilsKt.getShipObjectWorld((ServerLevel) level);
+            List<BlockPos> removal = new ArrayList<>();
+            for (Map.Entry<BlockPos, List<Integer>> entry : map.entrySet()) {
+                BlockPos blockPos = entry.getKey();
+                if(VSGameUtilsKt.isTickingChunk(level, new ChunkPos(blockPos)) && isAirOrFluid(level.getBlockState(blockPos))) {
+                    List<Integer> constraints = entry.getValue();
+                    for (Integer constraint : constraints) {
+                        serverShipWorldCore.removeConstraint(constraint);
+                    }
+                    removal.add(blockPos);
+                }
+            }
+            removal.forEach(map.keySet()::remove);
+        }
+
+        if(isBlockStateExtended() != this.wasBlockStateExtended) {
+            this.needUpdate = true;
+            this.wasBlockStateExtended = isBlockStateExtended();
+        }
+
+        if (this.needUpdate && this.wasBlockStateExtended) {
+
+            Vector3d selfPosOnShip = new Vector3d(getBlockPos().getX(), getBlockPos().getY(), getBlockPos().getZ()).add(0.5, 0.5, 0.5);
+            Quaterniondc rotationQuaternion = TransformUtilsKt.getDirectionToQuaterniond(getBlockState().getValue(FACING));
+            Vector3d attachmentOffset1 = rotationQuaternion.transform(new Vector3d(0.0, 0.5625, 0.0));
+            Vector3d attachmentLocalPos1 = selfPosOnShip.add(attachmentOffset1);
+
+            Vector3d find = VSGameUtilsKt.toWorldCoordinates(level, attachmentLocalPos1);
+            List<Vector3d> ships = VSGameUtilsKt.transformToNearbyShipsAndWorld(level, find.x, find.y, find.z, 0.25);
+            ships.add(find);
+
+            if(!level.isClientSide) {
+                ServerShipWorldCore serverShipWorldCore = VSGameUtilsKt.getShipObjectWorld((ServerLevel) level);
+                ServerShip selfShip = VSGameUtilsKt.getShipManagingPos((ServerLevel) level, getBlockPos());
+                for (Vector3d posOnShip : ships) {
+                    BlockPos blockPos = TransformUtilsKt.getToBlockPos(posOnShip);
+                    BlockState state = level.getBlockState(blockPos);
+                    if(isAirOrFluid(state))
+                        continue;
+                    ServerShip ship = VSGameUtilsKt.getShipManagingPos((ServerLevel) level, posOnShip);
+                    if(ship == selfShip)
+                        continue;
+                    long objectId1;
+                    long objectId2;
+                    Vector3d localPos1;
+                    Vector3d localPos2;
+                    Quaterniond localRot1;
+                    Quaterniond localRot2;
+                    if(selfShip==null) {
+                        objectId1 = ship.getId();
+                        objectId2 = serverShipWorldCore.getDimensionToGroundBodyIdImmutable().get(VSGameUtilsKt.getDimensionId(level));
+                        localPos1 = (Vector3d) ship.getTransform().getPositionInShip();
+                        localPos2 = (Vector3d) ship.getTransform().getPositionInWorld();
+                        localRot1 = (Quaterniond) ship.getTransform().getShipToWorldRotation();
+                        localRot2 = new Quaterniond();
+                    } else if (ship==null) {
+                        objectId1 = serverShipWorldCore.getDimensionToGroundBodyIdImmutable().get(VSGameUtilsKt.getDimensionId(level));
+                        objectId2 = selfShip.getId();
+                        localPos1 = (Vector3d) selfShip.getTransform().getPositionInWorld();
+                        localPos2 = (Vector3d) selfShip.getTransform().getPositionInShip();
+                        localRot1 = new Quaterniond();
+                        localRot2 = (Quaterniond) selfShip.getTransform().getShipToWorldRotation();
+                    } else {
+                        objectId1 = ship.getId();
+                        objectId2 = selfShip.getId();
+                        localPos1 = (Vector3d) ship.getTransform().getPositionInShip();
+                        localPos2 = selfShip.getTransform().getWorldToShip().transformPosition((Vector3d) ship.getTransform().getPositionInWorld());
+                        localRot1 = (Quaterniond) ship.getTransform().getShipToWorldRotation();
+                        localRot2 = (Quaterniond) selfShip.getTransform().getShipToWorldRotation();
+                    }
+                    VSAttachmentConstraint aconstraint = new VSAttachmentConstraint(objectId1, objectId2, this.compliance, localPos1, localPos2, this.maxForce, 0.0);
+                    VSFixedOrientationConstraint rconstraint = new VSFixedOrientationConstraint(objectId1, objectId2, this.compliance, localRot1.invert(new Quaterniond()), localRot2.invert(new Quaterniond()), this.maxForce);
+                    List<Integer> constraints = new ArrayList<>();
+                    Integer aconstraintId = serverShipWorldCore.createNewConstraint(aconstraint);
+                    Integer rconstraintId = serverShipWorldCore.createNewConstraint(rconstraint);
+                    constraints.add(aconstraintId);
+                    constraints.add(rconstraintId);
+                    this.map.put(blockPos, constraints);
+                }
+            }
+            if(level.isClientSide && ships.size() > 0) {
+                SuperGlueItem.spawnParticles(level, worldPosition, getBlockState().getValue(StickerBlock.FACING), true);
+                playClientSound(true);
+            }
+            this.needUpdate = false;
+        }
+        if (this.needUpdate) {
+            clearConstraint();
+            playClientSound(false);
+            this.needUpdate = false;
+        }
     }
 
     @Override
     public void remove() {
-        this.vs_addition$handler = null;
+        clearConstraint();
         super.remove();
     }
 
+    @Unique
+    public void clearConstraint() {
+        if(!level.isClientSide) {
+            ServerShipWorldCore serverShipWorldCore = VSGameUtilsKt.getShipObjectWorld((ServerLevel) level);
+            for (Map.Entry<BlockPos, List<Integer>> entry : map.entrySet()) {
+                List<Integer> constraints = entry.getValue();
+                for (Integer constraint : constraints) {
+                    serverShipWorldCore.removeConstraint(constraint);
+                }
+            }
+            map.clear();
+        }
+    }
+
+    @Unique
+    public boolean isAirOrFluid(BlockState state) {
+        return state.isAir() || state.getFluidState() != Fluids.EMPTY.defaultFluidState();
+    }
 
     /**
      * Safe wrapper for 'playSound', which is removed at runtime on fabric servers.
      */
-    @Unique
-    public Void vs_addition$playClientSound(boolean attach) {
-        if(this.level.isClientSide) {
+    public void playClientSound(boolean attach) {
+        if(level.isClientSide) {
             playSound(attach);
         }
-        return null;
     }
 
     @Environment(EnvType.CLIENT)
     @Shadow(remap = false)
-    public abstract void playSound(boolean attach);
+    protected abstract void playSound(boolean attach);
 }
 
